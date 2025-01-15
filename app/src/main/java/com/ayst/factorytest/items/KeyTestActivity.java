5ef4226c03dc614f0566fd0325c88dff1d4f1c97
LuoDongSheng <luodongsheng170@163.com>
2025-01-15 13:37:45 +0800

    按键测试调整

--- app/src/main/java/com/ayst/factorytest/items/KeyTestActivity.java
+++ app/src/main/java/com/ayst/factorytest/items/KeyTestActivity.java
@@ -36,9 +36,24 @@ import butterknife.BindView;
 public class KeyTestActivity extends ChildTestActivity {
     private static final String TAG = "KeyTestActivity";
 
-    private static final String PARAM_DEFAULT = "[{'name':'BACK', 'code': 4, 'state': 0}, " +
-            "{'name':'MENU', 'code': 82, 'state': 0}, " +
-            "{'name':'MUTE', 'code': 164, 'state': 0}, " +
+    private static final String PARAM_DEFAULT = "[{'name':'HOME', 'code': 110, 'state': 0}, " +
+            "{'name':'BACK', 'code': 4, 'state': 0}, " +
+            "{'name':'START', 'code': 108, 'state': 0}, " +
+            "{'name':'SELECT', 'code': 109, 'state': 0}, " +
+            "{'name':'A', 'code': 96, 'state': 0}, " +
+            "{'name':'B', 'code': 97, 'state': 0}, " +
+            "{'name':'X', 'code': 99, 'state': 0}, " +
+            "{'name':'Y', 'code': 100, 'state': 0}, " +
+            "{'name':'UP', 'code': 19, 'state': 0}, " +
+            "{'name':'LEFT', 'code': 21, 'state': 0}, " +
+            "{'name':'RIGHT', 'code': 22, 'state': 0}, " +
+            "{'name':'DOWN', 'code': 20, 'state': 0}, " +
+            "{'name':'L1', 'code': 102, 'state': 0}, " +
+            "{'name':'R1', 'code': 103, 'state': 0}, " +
+            "{'name':'L2', 'code': 104, 'state': 0}, " +
+            "{'name':'R2', 'code': 105, 'state': 0}, " +
+            "{'name':'L3', 'code': 106, 'state': 0}, " +
+            "{'name':'R3', 'code': 107, 'state': 0}, " +
             "{'name':'VOL+', 'code': 24, 'state': 0}, " +
             "{'name':'VOL-', 'code': 25, 'state': 0}]";
 
@@ -68,7 +83,6 @@ public class KeyTestActivity extends ChildTestActivity {
     @Override
     public void initViews() {
         super.initViews();
-
         mSuccessBtn.setVisibility(View.GONE);
 
         if (TextUtils.isEmpty(mTestItem.getParam())) {
